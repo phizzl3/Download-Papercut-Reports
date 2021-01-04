15caaa209681d1616b24b8c992c0b25c8373549c
Brandon <phizzl3@gmail.com>
2021-01-04 16:14:39 -0600

    Updated Docstring to match README

--- getreports/get_papercut_reports.py
+++ getreports/get_papercut_reports.py
@@ -2,17 +2,20 @@
 in as a single user to download reports from a Gmail account. 
 
 Additional requirements:
+
 * Gmail API will need to be enabled on account
-* credentials.json file from Gmail (stored in ./private)
-* token.pickle file from Gmail (stored in ./private)
-* data.py file containing search tags (stored in ./private)
-Instructions for the above can be found at:
+* credentials.json file from Gmail (Downloaded) (stored in ./private)
+* token.pickle file from Gmail (Generated) (stored in ./private)
+* data.py file containing search tags (stored in ./private)  
+
+Instructions for the above can be found at:  
 https://developers.google.com/gmail/api/quickstart/python
 
 What it does:
+
 * Connects to Gmail API
 * Checks for credentials & token
-    * Generates them if they don't exist
+  * Generates token if it doesn't exist using credentials from Gmail
 * Searches Gmail for Unread messages from PaperCut with attachments
 * Gets all of those messages
 * Downloads and renames the attachments based on location
